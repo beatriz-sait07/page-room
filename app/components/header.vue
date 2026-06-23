9f22504df3c403b7421431a87e9e7a786829ecb4
beatriz saito <beatriz.saito@play55.com.br>
2026-06-23 15:43:08 -0400

    featt: stage 1

---
+++ app/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <section></section>
+        <section class="grid grid-row justify-between"></section>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useMediaQuery } from '@vueuse/core'
+
+const isDesktop = useMediaQuery('(min-width: 768px)')
+const menuAberto = ref(false)
+</script>
+
+<style scoped></style>
