be6ac6e5bf1d2dc596f69c980703db2448dc11af
beatriz saito <beatriz.saito@play55.com.br>
2026-06-23 15:42:53 -0400

    init: projeto resetado, pronto para start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Room",
+      meta: [
+        {
+          name: "description",
+          content: "Landing page para propaganda de quartos",
+        },
+      ],
+    },
+  },
 
   modules: [
-    '@nuxt/a11y',
-    '@nuxtjs/tailwindcss',
-    'nuxt-lucide-icons',
-    '@nuxtjs/color-mode'
-  ]
-})
+    "@nuxt/a11y",
+    "@nuxtjs/tailwindcss",
+    "nuxt-lucide-icons",
+    "@nuxtjs/color-mode",
+  ],
+});
