2470e2a39b9813961e32664ba2e7a37f91170e94
beatriz saito <beatriz.saito@play55.com.br>
2026-06-25 15:00:24 -0400

    style: layout ok

--- app/app.vue
+++ app/app.vue
@@ -10,7 +10,7 @@
       </main>
       <Footer class="h-24 md:h-18 flex justify-center items-center flex-col gap-1 p-3">
         <p class="text-[8px] text-gray-800 dark:text-gray-200 ">
-          v{{ nuxtConfig.public.version }}
+          V{{ nuxtConfig.public.version }}
         </p>
         <p class="text-[8px] text-gray-800 dark:text-gray-200">
           Desenvolvido por Beatriz Saito - 2024
--- app/components/menuHamburguer.vue
+++ app/components/menuHamburguer.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="w-2/3 bg-black/80 z-1 h-screen absolute top-0 py-6 px-4 left-0" @click="toggleMenu">
-        <X @click="toggleMenu" class="text-gray-800 dark:text-gray-100 absolute left-3 top-6 -translate-y-1/2" />
+    <div class="w-2/3 bg-transparent backdrop-blur-lg shadow-lg shadow-gray-200 dark:shadow-gray-800 z-1 h-screen absolute top-0 py-6 px-4 left-0"
+        @click="toggleMenu">
+        <X @click="toggleMenu" :class="props.colorX || 'white'" class="absolute left-3 top-6 -translate-y-1/2" />
         <ul class="w-full grid grid-rows-3 gap-5 justify-center items-between text-gray-800 dark:text-gray-200">
             <li v-for="item in props.listaMenu" :key="item.name" class="relative text-center inline-block"
-                :class="props.listaMenu?.length ? 'border-b-2 border-gray-500 dark:border-gray-300' : ''">
+                :class="props.listaMenu?.length ? 'border-b-2 border-gray-700 dark:border-gray-300' : ''">
                 <NuxtLink :to="item.link">{{ item.name }}</NuxtLink>
             </li>
         </ul>
-        <p class="w-full text-[8px] text-gray-700 dark:text-gray-300 absolute bottom-3 text-center">v{{
+        <p class="w-full text-[8px] text-gray-300 dark:text-gray-700 absolute bottom-3 text-center">v{{
             nuxtConfig.public.version }}</p>
         <!-- backdrop-blur-md -->
     </div>
@@ -18,6 +19,7 @@ import { X } from '@lucide/vue';
 const props = defineProps<{
     menuAberto?: boolean
     listaMenu?: { name: string, link: string }[]
+    colorX?: string
 }>()
 
 const emit = defineEmits<{
